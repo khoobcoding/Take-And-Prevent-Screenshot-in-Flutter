f9521bc114318bcb62502641660f27d24d852d98
Khoob Coding <84430096+khoobcoding@users.noreply.github.com>
2023-09-26 07:06:38 +0530

    Add files via upload

---
+++ No_Screenshot.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:no_screenshot/no_screenshot.dart';
+
+class NoScreenshotApp extends StatefulWidget {
+  const NoScreenshotApp({super.key});
+
+  @override
+  State<NoScreenshotApp> createState() => _NoScreenshotAppState();
+}
+
+class _NoScreenshotAppState extends State<NoScreenshotApp>
+    with WidgetsBindingObserver {
+  NoScreenshot noscreenshot = NoScreenshot.instance;
+
+  @override
+  void initState() {
+    noscreenshot.screenshotOff();
+    WidgetsBinding.instance.addObserver(this);
+    super.initState();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      noscreenshot.screenshotOff();
+    }
+    if (state == AppLifecycleState.inactive) {
+      noscreenshot.screenshotOff();
+    }
+    if (state == AppLifecycleState.paused) {
+      noscreenshot.screenshotOff();
+    }
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 183, 35, 209),
+        title: const Text('No Screenshot App',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              style: const ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(
+                      Color.fromARGB(255, 183, 35, 209))),
+              child: const Text('Press to toggle screenshot'),
+              onPressed: () async {
+                // await noscreenshot.toggleScreenshot().then((value) {
+                //   print(value);
+                // });
+              },
+            ),
+            ElevatedButton(
+              style: const ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(
+                      Color.fromARGB(255, 183, 35, 209))),
+              child: const Text('Press to turn off screenshot'),
+              onPressed: () async {
+                // await noscreenshot.screenshotOff().then((value) {
+                //   print(value);
+                // });
+              },
+            ),
+            ElevatedButton(
+              style: const ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(
+                      Color.fromARGB(255, 183, 35, 209))),
+              child: const Text('Press to turn on screenshot'),
+              onPressed: () async {
+                // await noscreenshot.screenshotOn().then((value) {
+                //   print(value);
+                // });
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ Screenshot.dart
@@ -0,0 +1,157 @@
+import 'dart:io';
+import 'dart:math';
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:screenshot/screenshot.dart';
+
+class ScreenshotApp extends StatefulWidget {
+  const ScreenshotApp({super.key});
+
+  @override
+  State<ScreenshotApp> createState() => _ScreenshotAppState();
+}
+
+class _ScreenshotAppState extends State<ScreenshotApp> {
+  ScreenshotController screenshotcontroller=ScreenshotController();
+  Uint8List? myscreenshot;
+
+  savescreenshot(Uint8List image)async{
+    final direc=await getApplicationDocumentsDirectory();
+    final screenshotpath=File('${direc.path}/Screenshot - ${DateTime.now()}.png');
+    final result=await screenshotpath.writeAsBytes(image);
+    print('File Saved - $result');
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 36, 98, 254),
+        title: const Text('Screenshot App',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
+      ),
+      body: Screenshot(
+        controller: screenshotcontroller,
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 30,
+                ),
+                myscreenshot==null?Container(width: 200,height: 300,child: 
+                Center(child: Text('No Screenshot'),),)
+                : Container(decoration: BoxDecoration(
+                  border: Border.all(color: Color.fromARGB(255, 36, 98, 254)),
+                ),child: Image.memory(myscreenshot!,width: 200,height: 300,),),
+                const SizedBox(
+                  height: 30,
+                ),
+                MaterialButton(
+                    color: const Color.fromARGB(255, 36, 98, 254),
+                    onPressed: () async{
+                     myscreenshot= await screenshotcontroller.capture();
+                      savescreenshot(myscreenshot!);
+                    //  setState(() {
+                       
+                    //  });
+                    // screenshotcontroller.captureFromWidget(
+                    //   Container(color: Colors.indigo,
+                    //   width: 200,height: 200,
+                    //   padding: EdgeInsets.all(30),
+                    //   child: Center(child: Text('Pin - 68458925620')),)
+                    // ).then((value) {
+                    //   myscreenshot=value;
+                    //   setState(() {
+                        
+                    //   });
+                    // });
+                     final randomitemcount=Random().nextInt(99999);
+                     var mylongwidget=Builder(builder: (context) {
+                       return Container(
+                        padding: EdgeInsets.all(30),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.indigo,width: 5),
+                          color: Colors.redAccent
+                        ),
+                        child: Column(mainAxisSize: MainAxisSize.min,
+                        children: [
+                          for(int i=0;i<10;i++)
+                          Text('Tile Index - ${randomitemcount+i}')
+                        ],),
+                       );
+                     },);
+                    screenshotcontroller.captureFromLongWidget(
+                      InheritedTheme.captureAll(context, 
+                      Material(child: mylongwidget,)),context: context
+                    ).then((value) {
+                      myscreenshot=value;
+                      setState(() {
+                        
+                      });
+                    });
+                    savescreenshot(myscreenshot!);
+                    },
+                    child: const Text('Take Screenshot',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontSize: 20))),
+                MaterialButton(
+                    color: const Color.fromARGB(255, 36, 98, 254),
+                    onPressed: () async{
+                      // final direc=await getApplicationDocumentsDirectory();
+                      // await screenshotcontroller.captureAndSave(direc.path,
+                      // fileName: 'Screenshot - ${DateTime.now()}.png').then((value) {
+                      //   print('Screenshot Saved');
+                      // });
+                    },
+                    child: const Text('Take Screenshot & Save ',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontSize: 20))),
+                const SizedBox(
+                  height: 35,
+                ),
+                Container(
+                  height: 300,
+                  width: 250,
+                  color: Colors.indigo,
+                  child: const Center(
+                    child: Icon(
+                      Icons.g_translate_sharp,
+                      color: Colors.white,
+                      size: 150,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 35,
+                ),
+                Container(
+                  height: 300,
+                  width: 250,
+                  color: Colors.red,
+                  child: const Center(
+                    child: Icon(
+                      Icons.keyboard_command_key_sharp,
+                      color: Colors.white,
+                      size: 150,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
